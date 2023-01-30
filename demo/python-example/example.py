f2332903ac85af61ceb28aad0e6a2a0f3a64a946
Mateus Oliveira <msouzaol@redhat.com>
2023-01-30 15:45:40 -0300

    fixup! feat: Add prospector as Python code linter
    Signed-off-by: Mateus Oliveira <msouzaol@redhat.com>

--- demo/python-example/example.py
+++ demo/python-example/example.py
@@ -1,32 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
+from typing import Tuple
 
-response_file_bytes = b""
-response_file_txt = ""
+
+def read_file() -> Tuple[bytes, str]:
+    with open(Path(__file__).resolve().parent / "response.txt", "br") as file:
+        file_bytes = file.read()
+    return (file_bytes, file_bytes.decode("utf-8"))
 
 
 class ResponseHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    file_bytes, file_text = read_file()
+    print(f"Loaded response.txt with contents {file_text} (as bytes {file_bytes})")
+
+    def do_GET(self) -> None:  # noqa pylint:disable=invalid-name
         self.log_message(
-            f"Got request, returning {response_file_txt} "
-            f"(as bytes {response_file_bytes})"
+            f"Got request, returning {self.file_text} (as bytes {self.file_bytes})"
         )
         self.send_response(200, "PELORUS IS COOL")
         self.send_header("Content-Type", "text/plain")
-        self.send_header("Content-Length", str(len(response_file_bytes)))
+        self.send_header("Content-Length", str(len(self.file_bytes)))
         self.end_headers()
 
-        self.wfile.write(response_file_bytes)
+        self.wfile.write(self.file_bytes)
 
 
-def main():
-    global response_file_txt, response_file_bytes
-    with open("response.txt", "br") as f:
-        response_file_bytes = f.read()
-    response_file_txt = response_file_bytes.decode("utf-8")
-    print(
-        f"Loaded response.txt with contents {response_file_txt} "
-        f"(as bytes {response_file_bytes})"
-    )
+def main() -> None:
     server = HTTPServer(("", 8080), ResponseHandler)
     server.serve_forever()
 
